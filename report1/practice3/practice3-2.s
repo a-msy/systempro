7ed4dd3d7e2af9a7cd813e38d6bbeca1d7856ff7
Imada Masaya <m5s2y5a@gmail.com>
2019-10-28 17:00:47 +0900

    17:00

--- report1/practice3/practice3-2.s
+++ report1/practice3/practice3-2.s
@@ -1,17 +1,17 @@
         .text
         .align 2
 
-main:   addu    $t1,$zero,$zero
-        addu    $t0,$zero,$zero
+main:   li      $t1,$zero #t1を０で初期化
+        li      $t0,$zero #t0を０で初期化
         
-loop:   beq     $t1,5,print
-        li      $v0,5
+loop:   beq     $t1,2,print     #if(t1==5) go to print
+        li      $v0,5           #v0 read_int(input from user)
         syscall
-        addu    $t0,$t0,$v0
-        addu    $t1,$t1,1
-        j       loop
+        addu    $t0,$t0,$v0     #t0=t0+v0(return value)
+        addu    $t1,$t1,1       #t1++
+        j       loop            #jump to loop
         
-print:  li      $v0,1
-        move    $a0,$t0
+print:  li      $v0,1           #print_int
+        move    $a0,$t0         #a0 <= t0 (引数)
         syscall
-        j       $ra
+        j       $ra             #return 0
