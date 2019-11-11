56732548cab93060c708ae8ebbca7ccfe18372b1
Imada Masaya <m5s2y5a@gmail.com>
2019-11-11 15:44:44 +0900

    15:44

---
+++ report1/kadai/sosu1.s
@@ -0,0 +1,91 @@
+        .data
+        .align 2
+space:
+        .asciiz " "
+enter:
+        .asciiz "\n"
+        
+        .text
+        .align 2
+        
+test_prime:
+        subu    $sp,$sp,32 #stackpointer
+        sw      $ra,20($sp) #$ra
+        sw      $fp,16($sp) #flamepointer
+        addiu   $fp,$sp,28  #set fp
+
+        li      $t0,2   # 1 is not prime
+prime_for:
+        beq     $t0, $a0, return1   # return1 if n is prime number (i==n)
+        bgt     $t0, $a0, prime_exit    # break the for if n > i
+        rem     $t1, $a0, $t0       # $t1 = n % i
+        beqz    $t1, prime_exit     # goto Exit_prime if $t1 == 0
+        addi    $t0, $t0, 1         # increment i
+        j       prime_for           # loop again with incremented i
+return1:
+        li      $v0,1       #if number is prime
+        lw      $ra,20($sp)
+        lw      $fp,16($sp)
+        addiu   $sp,$sp,32
+        j       $ra
+prime_exit:
+        li      $v0,0       #if number is not prime
+        lw      $ra,20($sp)
+        lw      $fp,16($sp)
+        addiu   $sp,$sp,32
+        j       $ra
+
+
+main:
+        subu    $sp,$sp,32 #stackpointer
+        sw      $ra,20($sp) #$ra
+        sw      $fp,16($sp) #flamepointer
+        addiu   $fp,$sp,28  #set fp
+
+        li      $s0,100     #number of max(match<100)
+        li      $s1,0       #number of loop(match)
+        li      $s2,2       #number to check and print(n=2)
+        li      $s3,1       #test_prime(n) == $s3
+        li      $s4,10       #if 10,print  \n   
+while:  
+        beq     $s0,$s1,exit # s1 == 100 => exit
+        move    $a0,$s2     # $s3 => $a0
+        jal     test_prime
+        
+        bne     $v0,$s3,else    # $v0 != 1
+        move    $a0,$s2        # put the prime number
+        li      $v0,1
+        syscall
+        
+        la      $a0, space      # " "
+        li      $v0,4
+        syscall
+        
+        addiu   $s1,$s1,1   #n++
+        
+        rem     $t2, $s1, $s4    # $t2 = n % i
+        beqz    $t2, print_enter     # goto printenter if $t2 == 0
+        
+else:   
+        addiu   $s2, $s2, 1     # n = n + 1 
+        j       while            # go to Loop
+exit:   
+        lw      $ra, 20($sp)  # Restore return address
+        lw      $fp, 16($sp)  # Restore frame pointer
+        addiu   $sp, $sp, 32  # Pop stack frame
+        j       $ra # End this program
+        
+print_enter:
+        subu    $sp,$sp,32 #stackpointer
+        sw      $ra,20($sp) #$ra
+        sw      $fp,16($sp) #flamepointer
+        addiu   $fp,$sp,28  #set fp
+
+        la      $a0,enter
+        li      $v0,4
+        syscall
+
+        lw      $ra, 20($sp)  # Restore return address
+        lw      $fp, 16($sp)  # Restore frame pointer
+        addiu   $sp, $sp, 32  # Pop stack frame
+        j       $ra #return
